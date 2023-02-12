check the fape loss and fix the rotation matrix bug

esm_inpaint/modules.py
@@ -100,12 +100,12 @@ class esm_inpaint(nn.Module):
         # convert the coord to global frames
         bb_frame_atom = coord[:,:,0:3,:]
         bb_rotation,bb_translation = utils.get_bb_frames(bb_frame_atom)
-        mask_frame = mask.reshape(*mask.shape,1,1)
-        mask_frame = mask_frame.expand(*mask_frame.shape[:-2],4,4)
+        # mask_frame = mask.reshape(*mask.shape,1,1)
+        # mask_frame = mask_frame.expand(*mask_frame.shape[:-2],4,4)
         bb_frame = torch.zeros((*bb_rotation.shape[:-2],4,4),device=coord.device)
         bb_frame[...,:3,:3] = bb_rotation
         bb_frame[...,:3,3] = bb_translation # [B, L, 4, 4]
-        bb_frame = bb_frame * mask_frame
+        # bb_frame = bb_frame * mask_frame
         bb_frame = Rigid.from_tensor_4x4(bb_frame)
 
         # running the esmfold 

esm_inpaint/train_poc.py
@@ -2,7 +2,7 @@
 # date   : 12.28
 # Author : Bo Zhang
 # Content: 
-#  - 4 V100 GPU for poc :5 epochs, batch_size = 8 
+#  - 4 V100 GPU for poc :1 epochs, batch_size = 8 
 #  - Training:16631, Validation:1516, Test:1864
 # Visualization : wanb
 
@@ -28,9 +28,9 @@ def get_args():
     parser.add_argument('--temperature', type=float, default=1.0, help='Temperature to sample an amino acid')
     parser.add_argument('--noise', type=float, default=0.0, help='Add noise in training')
     parser.add_argument('--lr',type=float,default=1e-3, help="learning rate of Adam optimizer")
-    parser.add_argument('--project_name', type=str,default="poc-esm-inpaint",help="project name in wandb")
-    parser.add_argument('--data_jsonl', type=str,default="/root/data/chain_set.jsonl",help='Path for the jsonl data')
-    parser.add_argument('--split_json', type=str,default="/root/data/splits.json", help='Path for the split json file')
+    parser.add_argument('--project_name', type=str,default="esm-inpaint",help="project name in wandb")
+    parser.add_argument('--data_jsonl', type=str,default="/data/users/zb/data/chain_set.jsonl",help='Path for the jsonl data')
+    parser.add_argument('--split_json', type=str,default="/data/users/zb/data/splits.json", help='Path for the split json file')
     parser.add_argument('--save_folder',type=str,default="./",help="output folder for the model parameters")
     parser.add_argument('--output_folder',type=str,default="./",help="output folder for the model print information")
     parser.add_argument('--parameter_pattern',type=str,default="min",help="which part of parameters should be frozen")
@@ -333,10 +333,13 @@ def ddp_main(args):
             # seq loss (nll)
             log_pred = output['log_softmax_aa']
             padding_mask = batch['padding_mask']
+
+            pre_seq = log_pred.argmax(dim=-1)
+            recovery = torch.sum( (pre_seq == batch['seq']) * padding_mask ) / torch.sum(padding_mask)
+
             loss_seq_token, loss_seq = utils.loss_nll(
                 batch['seq'], log_pred, padding_mask)
             ppl = np.exp(loss_seq.cpu().data.numpy())
-
             # structure loss (fape)
             B, L = output['positions'].shape[:2]
             pred_position = output['positions'].reshape(B, -1, 3)
@@ -344,7 +347,7 @@ def ddp_main(args):
             position_mask = torch.ones_like(target_position[..., 0])
             fape_loss = torch.mean(utils.compute_fape(
                 output['pred_frames'], output['target_frames'], batch['padding_mask'], pred_position, target_position, position_mask, 10.0))
-
+            lddt = utils.lddt_ca(pred_position,target_position,padding_mask.unsqueeze(-1))
             # loss = 0.5*fape + 2*cse copy from DeepMind AlphaFold2 loss function
             loss = 2 * loss_seq + 0.5 * fape_loss
             # add the backward loss
@@ -356,15 +359,15 @@ def ddp_main(args):
 
             # log the loss terms
             if local_rank == 0:
-                metric = {"TRAIN/ppl": ppl.item(), 'TRAIN/fape': fape_loss.item(),
-                          "TRAIN/ave_loss": loss.item(), "TRAIN/epoch": epoch}
+                metric = {"TRAIN/ppl": ppl.item(), "TRAIN/fape": fape_loss.item(),
+                          "TRAIN/ave_loss": loss.item(), "TRAIN/recovery": recovery.item(),"TRAIN/epoch": epoch}
                 wandb.log(metric)
     param_state_dict = ddp_model.module.inpaint_state_dict()
     torch.save({
             'epoch': epoch,
             'model_state_dict': param_state_dict,
             'optimizer_state_dict': optimizer.state_dict()
-        }, os.path.join(args.save_folder, f"test.pt"))
+        }, os.path.join(args.save_folder, f"weight_02.pt"))
                 
     #     # Validation epoch only for rank0, remember validation data loader is the normal sampler
     #     if local_rank == 0:

esm_inpaint/utils.py
@@ -22,6 +22,14 @@ import torch.nn.functional as F
 import torch.utils.data as data
 from typing import Sequence, Tuple, List, Optional,Iterable
 from openfold.utils.rigid_utils import Rotation, Rigid
+from openfold.np import residue_constants
+from openfold.utils.tensor_utils import (
+    tree_map,
+    tensor_tree_map,
+    masked_mean,
+    permute_final_dims,
+    batched_gather,
+)
 from esm.data import BatchConverter
 
 
@@ -214,12 +222,10 @@ def transform(v, R, t=None):
         t: translation vector, (length x batch_size x 3)
     Returns:
         Rotated version of v by rotation matrix R.
-    Explain:
-        As R is a rotation matrix, torch.sum(v * R, dim = -2) is a  transpose matrix to (R @ v).squeeze(-1)
     """
     R = R.unsqueeze(-3)  # [B, L, 3, 3]        -> [B, L, 1, 3, 3]
     v = v.unsqueeze(-1)  # [B, L, channels, 3] -> [B, L, channels, 3 ,1 ]
-    v_rotation = torch.sum(v * R, dim=-2)
+    v_rotation = (R @ v).squeeze(-1)
     if t is not None:
         v_translation = v_rotation + t
     return v_translation
@@ -240,11 +246,33 @@ def get_bb_frames(coords):
     e1 = normalize(v1, dim=-1)
     u2 = v2 - e1 * torch.sum(e1 * v2, dim=-1, keepdim=True)
     e2 = normalize(u2, dim=-1)
-    e3 = torch.cross(e1, e2, dim=-1)
-    R = torch.stack([e1, e2, e3], dim=-2)
+    e3 = torch.cross(e1, e2, dim = -1)
+    R = torch.stack([e1, e2, e3], dim=-1) # note dim = -1 !!!
     t = coords[:, :, 1]  # translation is just the CA atom coordinate
     return R, t
 
+
+
+def rot_to_quat(
+    rot: torch.Tensor,
+):
+    if(rot.shape[-2:] != (3, 3)):
+        raise ValueError("Input rotation is incorrectly shaped")
+
+    rot = [[rot[..., i, j] for j in range(3)] for i in range(3)]
+    [[xx, xy, xz], [yx, yy, yz], [zx, zy, zz]] = rot 
+
+    k = [
+        [ xx + yy + zz,      zy - yz,      xz - zx,      yx - xy,],
+        [      zy - yz, xx - yy - zz,      xy + yx,      xz + zx,],
+        [      xz - zx,      xy + yx, yy - xx - zz,      yz + zy,],
+        [      yx - xy,      xz + zx,      yz + zy, zz - xx - yy,]
+    ]
+
+    k = (1./3.) * torch.stack([torch.stack(t, dim=-1) for t in k], dim=-2)
+
+    _, vectors = torch.linalg.eigh(k)
+    return vectors[..., -1]
 
 def norm(tensor, dim, eps=1e-8, keepdim=False):
     """
@@ -269,6 +297,7 @@ def nan_to_num(ts, val=0.0):
     """
     val = torch.tensor(val, dtype=ts.dtype, device=ts.device)
     return torch.where(~torch.isfinite(ts), val, ts)
+
 
 
 
@@ -423,6 +452,83 @@ def backbone_loss(
     fape_loss = torch.mean(fape_loss)
 
     return 
+
+def lddt(
+    all_atom_pred_pos: torch.Tensor,
+    all_atom_positions: torch.Tensor,
+    all_atom_mask: torch.Tensor,
+    cutoff: float = 15.0,
+    eps: float = 1e-10,
+    per_residue: bool = True,
+) -> torch.Tensor:
+    n = all_atom_mask.shape[-2]
+    dmat_true = torch.sqrt(
+        eps
+        + torch.sum(
+            (
+                all_atom_positions[..., None, :]
+                - all_atom_positions[..., None, :, :]
+            )
+            ** 2,
+            dim=-1,
+        )
+    )
+
+    dmat_pred = torch.sqrt(
+        eps
+        + torch.sum(
+            (
+                all_atom_pred_pos[..., None, :]
+                - all_atom_pred_pos[..., None, :, :]
+            )
+            ** 2,
+            dim=-1,
+        )
+    )
+    dists_to_score = (
+        (dmat_true < cutoff)
+        * all_atom_mask
+        * permute_final_dims(all_atom_mask, (1, 0))
+        * (1.0 - torch.eye(n, device=all_atom_mask.device))
+    )
+
+    dist_l1 = torch.abs(dmat_true - dmat_pred)
+
+    score = (
+        (dist_l1 < 0.5).type(dist_l1.dtype)
+        + (dist_l1 < 1.0).type(dist_l1.dtype)
+        + (dist_l1 < 2.0).type(dist_l1.dtype)
+        + (dist_l1 < 4.0).type(dist_l1.dtype)
+    )
+    score = score * 0.25
+
+    dims = (-1,) if per_residue else (-2, -1)
+    norm = 1.0 / (eps + torch.sum(dists_to_score, dim=dims))
+    score = norm * (eps + torch.sum(dists_to_score * score, dim=dims))
+
+    return score
+
+def lddt_ca(
+    all_atom_pred_pos: torch.Tensor,
+    all_atom_positions: torch.Tensor,
+    all_atom_mask: torch.Tensor,
+    cutoff: float = 15.0,
+    eps: float = 1e-10,
+    per_residue: bool = True,
+) -> torch.Tensor:
+    ca_pos = residue_constants.atom_order["CA"]
+    all_atom_pred_pos = all_atom_pred_pos[..., ca_pos, :]
+    all_atom_positions = all_atom_positions[..., ca_pos, :]
+    # all_atom_mask = all_atom_mask[..., ca_pos : (ca_pos + 1)]  # keep dim
+
+    return lddt(
+        all_atom_pred_pos,
+        all_atom_positions,
+        all_atom_mask,
+        cutoff=cutoff,
+        eps=eps,
+        per_residue=per_residue,
+    )
 
 class CoordBatchConverter(BatchConverter):
     def __init__(self, alphabet, truncation_seq_length: int = None):