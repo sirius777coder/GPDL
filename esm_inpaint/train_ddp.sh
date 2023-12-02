b885bb574529696e27bfb84c26138c4cc28284cf
stu005 <936314117@qq.com>
2023-12-02 15:45:37 +0800

    remove old name folder

--- esm_inpaint/train_ddp.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# python3 -m torch.distributed.run --nproc_per_node=4 --nnodes=1 --node_rank=0 --master_addr="192.168.0.1" --master_port=12345 train_poc.py 
-python3 -m torch.distributed.launch --nproc_per_node=4 train_poc.py
--- esm_inpaint/train_poc.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-python3 train_poc.py --output_folder parameters/ \
-    --project_name "poc-esm-inpaint_v3" \
-    --epochs 2 \
-    --batch_size 4 \
-    --max_length 200 \
-    --lr 0.01 \
